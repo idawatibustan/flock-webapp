5a2c2e903fb5ca7bf591fe4d8874c036ffb1071d
andre <tanandre93@gmail.com>
2017-02-11 22:10:27 -0800

    change implementation of questions from list to dic

--- helper.py
+++ helper.py
@@ -3,9 +3,9 @@ def get_questions(params=None):
     questions = json.loads(open('json/questions.json').read())
     if params:
         for param, val in params.iteritems():
-            questions = [question for question in questions if question[param] == val]
-        sophisticated_sort(res)
-        questions = sorted(questions, key=lambda x: x['rank'], reverse=True)
+            questions = { q_id: obj for q_id, obj in questions.iteritems() if obj[param] == val }
+    questions = [v for v in questions.values()]
+    questions = sorted(questions, key=lambda x: x['rank'], reverse=True)
     return questions
 
 def process_query(query):
@@ -58,14 +58,5 @@ def process_query(query):
 def save_question(q_obj):
     questions = json.loads(open('json/questions.json').read())
     q_id = q_obj['q_id']
-    ind = None
-    # holy shit this is so inefficient
-    for i, obj in enumerate(questions):
-        if dic['q_id'] = q_id:
-            ind = i
-            break
-    if ind:
-        questions[ind] = q_obj
-    else:
-        questions.append(q_obj)
+    questions[q_id] = q_obj
     open('json/questions.json', 'w').write(json.dumps(questions, indent=4))
